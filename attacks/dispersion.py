08e6a00d274df7fe92f62877324b90ddcd544ed8
lyt910522 <lyt910522@gmail.com>
2019-04-18 18:25:23 +0000

    .

--- attacks/dispersion.py
+++ attacks/dispersion.py
@@ -2,32 +2,36 @@ import copy
 import numpy as np
 from torch.autograd import Variable
 import torch
+import torch.nn.functional as F
 
 import pdb
 
 class DispersionAttack(object):
-    def __init__(self, model, epsilon=0.01, steps=100):
-
+    def __init__(self, model, epsilon=0.063, step_size=0.004, steps=100, regularization_weight=1):
+        
+        self.step_size = step_size
         self.epsilon = epsilon
         self.steps = steps
         self.model = copy.deepcopy(model)
+        self.regularization_weight = regularization_weight
 
     def __call__(self, X_nat, internal=[]):
         """
         Given examples (X_nat, y), returns adversarial
         examples within epsilon of X_nat in l_infinity norm.
         """
-        X_nat_np = X_nat.numpy()
+        X_nat_np = X_nat.cpu().numpy()
         for p in self.model.parameters():
             p.requires_grad = False
         
         X = np.copy(X_nat_np)
         
         for i in range(self.steps):
+            X_nat_var = Variable(torch.from_numpy(X_nat_np).cuda(), requires_grad=False, volatile=False)
             X_var = Variable(torch.from_numpy(X).cuda(), requires_grad=True, volatile=False)
             internal_logits, pred = self.model.prediction(X_var, internal=internal)
-            logit = internal_logits[-1]
-            loss = logit.std()
+            logit = internal_logits[14]
+            loss = logit.std() #+ self.regularization_weight * F.l1_loss(X_nat_var, X_var, reduction='mean')
             if(i % 20 == 0):
                 print(loss)
             self.model.zero_grad()
@@ -35,7 +39,8 @@ class DispersionAttack(object):
             grad = X_var.grad.data.cpu().numpy()
             X_var.grad.zero_()
 
-            X -= self.epsilon * np.sign(grad)
+            X -= self.step_size * np.sign(grad)
+            X = np.clip(X, X_nat_np - self.epsilon, X_nat_np + self.epsilon)
             X = np.clip(X, 0, 1) # ensure valid pixel range
 
         return torch.from_numpy(X)
--- sample.py
+++ sample.py
@@ -7,21 +7,40 @@ from image_utils import numpy_to_bytes
 from models.vgg import Vgg16
 from models.resnet import Resnet152
 from attacks.dispersion import DispersionAttack
+from attacks.mifgsm import MomentumIteratorAttack
 from api_utils import detect_label_numpy
 import numpy as np
-
+import torchvision
+import pdb
 
 # Resnet152 [4, 5, 6, 7]
 # Vgg16 [2, 7, 14, 21, 28]
-image = load_image(data_format='channels_first', fname='origin.jpg')
-image = numpy_to_variable(image)
+image_np = load_image(data_format='channels_first', fname='origin.jpg')
+image = numpy_to_variable(image_np)
 
-model = Resnet152()
-attack = DispersionAttack(model, steps=6 00)
-internal = [4, 5, 6, 7]
+'''
+model = torchvision.models.vgg16(pretrained=True).cuda()
+attack = MomentumIteratorAttack(model, decay_factor=0.5, epsilon=60./255, steps=1000, step_size=1./255, random_start=False)
+image_torch_nchw = torch.from_numpy(np.expand_dims(image_np, axis=0)).float()
+pred_nat = model(image_torch_nchw.cuda()).detach().cpu().numpy()
+label = np.argmax(pred_nat)
+label_tensor = torch.tensor(np.array([label]))
+adv = attack(image_torch_nchw, label_tensor)
+'''
+
+
+model = Vgg16()
+internal = [i for i in range(29)]
+attack = DispersionAttack(model, epsilon=60./255, step_size=1./255, steps=1000)
 adv = attack(image, internal=internal)
+
+
+
 adv_np = variable_to_numpy(adv)
 image = numpy_to_bytes(adv_np)
 ret = detect_label_numpy(image)
 print(ret)
 save_image(adv_np)
+
+linf = int(np.max(abs(image_np - adv_np)) * 255)
+print('linf: ', linf)
