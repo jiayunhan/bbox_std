8e30c954e96db088dee0c5d865e278091303aa32
lyt910522 <lyt910522@gmail.com>
2019-10-08 19:47:20 +0000

    .

--- run_gpu4.sh
+++ run_gpu4.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python script_generate_adversarial_imagenet5000.py --adv-method dr -tm inception_v3 --inc3-attacklayer 7 --step-size 4 --steps 100
+CUDA_VISIBLE_DEVICES=4 python script_generate_adversarial_imagenet5000.py --adv-method dr -tm inception_v3 --inc3-attacklayer 7 --step-size 4 --steps 100
--- run_gpu7.sh
+++ run_gpu7.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python script_generate_adversarial_imagenet5000.py --adv-method dr -tm inception_v3 --inc3-attacklayer 4 --step-size 4 --steps 100
+CUDA_VISIBLE_DEVICES=7 python script_generate_adversarial_imagenet5000.py --adv-method dr -tm inception_v3 --inc3-attacklayer 4 --step-size 4 --steps 100
