70978d63e2b22025602bb5a9e05cbbad08294df4
lyt910522 <lyt910522@gmail.com>
2019-10-10 02:15:17 +0000

    .

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=2 python script_generate_adversarial_imagenet5000.py --dataset-dir ~/yantao/imagenet5000 --adv-method dr -tm inception_v3 --inc3-attacklayer 12 --step-size 4 --steps 100
+GPU_IDS=(0 1 2 3)
+ATTACK_LAYER=(4 5 6 7)
+
+for ((i=0; i<4; i++))
+do
+    echo CUDA_VISIBLE_DEVICES=${GPU_IDS[$i]} python script_generate_adversarial_imagenet5000.py --dataset-dir ~/imagenet5000 --adv-method dr -tm resnet152 --res152-attacklayer ${ATTACK_LAYER[$i]} --step-size 2 --steps 500
+done
